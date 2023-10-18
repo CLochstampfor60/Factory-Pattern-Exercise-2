65ab91bbc68d1f78ac95d4d305ae93143da3b60f
CLochstampfor60 <cflochjr@gmail.com>
2023-10-18 14:47:37 -0400

    20% complete

---
+++ FactoryPatternExercise2/MongoDataAccess.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryPatternExercise2
+{
+    internal class MongoDataAccess : IDataAccess
+    {
+        public void LoadData()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SaveData()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
